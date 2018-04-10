1fdc9b980347eb185cbd759368f63daa1d2de1aa
Adam Silverstein <adam@10up.com>
2018-04-10 08:58:16 -0400

    Support an optional namespace parameter for hasAction/hasFilter

--- packages/hooks/src/createHasHook.js
+++ packages/hooks/src/createHasHook.js
@@ -5,17 +5,24 @@
  * @param  {Object}   hooks Stored hooks, keyed by hook name.
  *
  * @return {Function}       Function that returns whether any handlers are
- *                          attached to a particular hook.
+ *                          attached to a particular hook and optional namespace.
  */
 function createHasHook( hooks ) {
 	/**
 	 * Returns how many handlers are attached for the given hook.
 	 *
-	 * @param  {string}  hookName The name of the hook to check for.
+	 * @param  {string}  hookName  The name of the hook to check for.
+	 * @param  {string}  namespace Optional. The unique namespace identifying the callback in the form `vendor/plugin/function`.
 	 *
 	 * @return {boolean} Whether there are handlers that are attached to the given hook.
 	 */
-	return function hasHook( hookName ) {
+	return function hasHook( hookName, namespace ) {
+
+		// Use the namespace if provided.
+		if ( 'undefined' !== typeof namespace ){
+			return hookName in hooks && namespace in hooks[ hookName ];
+		}
+
 		return hookName in hooks;
 	};
 }
