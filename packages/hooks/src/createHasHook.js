5e83e1f7f9b38870cd0403e0c878909c293002e9
Adam Silverstein <adam@10up.com>
2018-04-11 11:32:13 -0400

    hasHook - enable optional namespace as part of hookName

--- packages/hooks/src/createHasHook.js
+++ packages/hooks/src/createHasHook.js
@@ -1,3 +1,5 @@
+import extractNamespace from './extractNamespace.js';
+
 /**
  * Returns a function which, when invoked, will return whether any handlers are
  * attached to a particular hook.
@@ -5,17 +7,29 @@
  * @param  {Object}   hooks Stored hooks, keyed by hook name.
  *
  * @return {Function}       Function that returns whether any handlers are
- *                          attached to a particular hook.
+ *                          attached to a particular hook and optional namespace.
  */
 function createHasHook( hooks ) {
 	/**
-	 * Returns how many handlers are attached for the given hook.
+	 * Returns whether any handlers are attached for the given hookName and optional namespace.
 	 *
-	 * @param  {string}  hookName The name of the hook to check for.
+	 * @param {string}   hookName  Name of hook to add. Optionally, use a period to add a
+	 * namespace identifying the callback in the form `hookName.vendor/plugin/function`.
 	 *
 	 * @return {boolean} Whether there are handlers that are attached to the given hook.
 	 */
 	return function hasHook( hookName ) {
+
+		// Extract the namespace, if provided.
+		const namespace = extractNamespace( hookName );
+
+		// Use the namespace if provided.
+		if ( 'undefined' !== typeof namespace ){
+			return hookName in hooks &&
+				hooks[ hookName ].handlers &&
+				hooks[ hookName ].handlers.map( hook => hook['namespace'] ).includes( namespace );
+		}
+
 		return hookName in hooks;
 	};
 }
