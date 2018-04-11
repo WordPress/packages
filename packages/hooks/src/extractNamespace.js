757bfdb8d915b6f3a99a054768fb60770b59f3d6
Adam Silverstein <adam@10up.com>
2018-04-11 12:28:29 -0400

    exit early from extractNamespace if hookName isn’t a string

--- packages/hooks/src/extractNamespace.js
+++ packages/hooks/src/extractNamespace.js
@@ -3,12 +3,15 @@
  *
  * @param  {string} hookName The hook+namespace name to extract the namespace from.
  *                           Should be a non empty string containing only numbers, letters,
- *                           dashes, periods and underscores. Also, the hook name cannot
+ *                           dashes and underscores. Also, the hook name cannot
  *                           begin with `__`. Takes the form `hookName.vendor/plugin/function`.
  *
- * @return {bool}            Whether the hook name is valid.
+ * @return {string}          The namespace, if provided.
  */
 function extractNamespace( hookName ) {
+	if ( 'string' !== typeof hookName ) {
+		return hookName;
+	}
 	return hookName.split( '.' )[1];
 }
 
